a5bdd1db6b5ed0b679d03e68a8f41d0694a411e9
Prashant Rizal <prsnt.rizal@gmail.com>
2025-04-06 23:21:39 +1000

    Unnecessary files

--- obj/Debug/net8.0/NumericTicTacToe.AssemblyInfo.cs
+++ obj/Debug/net8.0/NumericTicTacToe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumericTicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152a1d6aff292f0ebcedca37543040c4f5de53fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0a7f8ce4814ba0be6f82bc94642cc89e934203")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumericTicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumericTicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
